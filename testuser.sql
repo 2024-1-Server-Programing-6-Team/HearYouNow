1c800d97cf955940ea1a18c139017cce971da4e4
jyoh5005 <jyoh1005@gmail.com>
2024-05-31 15:39:24 +0900

    test user sql

---
+++ testuser.sql
@@ -0,0 +1,7 @@
+USE HearYouNow;
+
+INSERT INTO `user` (user_id, user_name, password, email, age) VALUES (1, 'Alice', '123', 'alice@example.com', 25);
+INSERT INTO `user` (user_id, user_name, password, email, age) VALUES (2, 'Bob', '123', 'bob@example.com', 30);
+INSERT INTO `user` (user_id, user_name, password, email, age) VALUES (3, 'Charlie', '123', 'charlie@example.com', 35);
+
+COMMIT;
